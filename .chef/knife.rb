6d41d5b3775ea2e44924f3c710b82bb691143f30
root <root@chef.totvs.com.br>
2015-01-19 15:56:22 -0200

    more changes

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name "leandro"
 client_key "#{current_dir}/user.key"
 validation_client_name "leandro"
 validation_key "#{current_dir}/org.key"
-chef_server_url "https://chef/organizations/totvs"
+chef_server_url "https://chef.totvs.com.br/organizations/totvs"
 cache_type 'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path ["#{current_dir}/../cookbooks"]
